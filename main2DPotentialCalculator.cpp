85a45904a31b9925d82076d1fa027c32a51e5314
gdideron <guillaumedideron@Gmail.com>
2015-05-09 17:02:20 +0100

    add array filling code

--- main2DPotentialCalculator.cpp
+++ main2DPotentialCalculator.cpp
@@ -1,24 +1,55 @@
+#include <iostream>
+
+//Define constants
+//number of ierations of the potential calculation
+//dimensions of the array
+
+int main() {
+    
+    //Define constants
+    const int iterations = 10;//number of ierations of the potential calculation
+    const int N = 10, M = 10;//dimensions of the array
+
+    
+    
 // Create a NxN array that represents the grid
-// double array N,M
+    double grid[N][M];
 
-
-
+    
+    
+    
 // Fill the array with 0s but for points associated with
 // the bottom line of the grid (ground). Those are filled with 1s
-
 // First going through the height of the array
-// for i from zero to N
-
-// Then through the width of the array
-// for j from 0 to M
+    for (int i = 0 ; i < N ; i++ ){ //i is vertical coordinate, N heigth
+        
+        // Then through the width of the array
+        for (int j = 0 ; j < M ; j++){ //j is horizontal coordinate, M width
 
 // Set condition if it is the bottom line
-// Then make the point equal to 1
+            if (i == 0)  grid [i][j] = 1;// Then make the point equal to 1
+            
+                // Otherwise set point to 0
+                else grid[i][j] = 0;
 
-// Otherwise set point to 0
+        } //end width loop
+    }// end height loop
+    
+    
+    
 
+// For testing, print the array calculated
+ /*
+    for (int i = 0 ; i < N ; i++ ){
+        for (int j = 0 ; j < M ; j++){
+    std::cout << grid[i][j];
 
-
+        }
+    }
+    
+*/
+    
+    
 // Go through the array again
 
 // First going through the height of the array minus 1
@@ -47,6 +78,8 @@
 
 //iterate the process an arbitrary number of times
 
+    return 0;
+}
 
 //comments:
 /*
