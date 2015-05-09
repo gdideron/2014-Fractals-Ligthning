523b24debe516910622cf88e186470295c03c686
gdideron <guillaumedideron@Gmail.com>
2015-05-09 16:13:13 +0100

    added top level plan as discussed on Friday 8

---
+++ main2DPotentialCalculator.cpp
@@ -0,0 +1,22 @@
+// Create a NxN array that represents the grid
+
+// Fill the array with 0s but for points associated with
+// the bottom line of the grid (ground) that are filled with 1s
+
+// Go through the array, take the potential of teh four surrouding
+// points and average them.
+// Change value of point to that average
+
+//iterate the process an arbitrary number of times
+
+
+//comments:
+/*
+ -break tasks down into functions to call from the main file
+ 
+ -start with a finite number of iterations before moving on to
+ recognising convergence
+ 
+ -later allow the size of array dimensions to be flexible to 
+ allow for further investigation
+ */
