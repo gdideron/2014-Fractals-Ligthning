884ce3805f9bf5513da2f45c1ee21faf1036ce73
gdideron <guillaumedideron@Gmail.com>
2015-05-09 18:01:07 +0100

    Add code for the potential calculation

--- main2DPotentialCalculator.cpp
+++ main2DPotentialCalculator.cpp
@@ -8,26 +8,25 @@ int main() {
     
     //Define constants
     const int iterations = 10;//number of ierations of the potential calculation
-    const int N = 10, M = 10;//dimensions of the array
+    const int height = 10, width = 10;//dimensions of the array
 
     
     
 // Create a NxN array that represents the grid
-    double grid[N][M];
+    double grid[height][width];
 
-    
     
     
 // Fill the array with 0s but for points associated with
 // the bottom line of the grid (ground). Those are filled with 1s
 // First going through the height of the array
-    for (int i = 0 ; i < N ; i++ ){ //i is vertical coordinate, N heigth
+    for ( int i = 0 ; i < height ; i++ ){ //i is vertical coordinate, N heigth
         
         // Then through the width of the array
-        for (int j = 0 ; j < M ; j++){ //j is horizontal coordinate, M width
+        for ( int j = 0 ; j < width ; j++ ){ //j is horizontal coordinate, M width
 
 // Set condition if it is the bottom line
-            if (i == 0)  grid [i][j] = 1;// Then make the point equal to 1
+            if ( i == 0 )  grid [i][j] = 1;// Then make the point equal to 1
             
                 // Otherwise set point to 0
                 else grid[i][j] = 0;
@@ -35,54 +34,79 @@ int main() {
         } //end width loop
     }// end height loop
     
-    
-    
 
+    
+   /*
 // For testing, print the array calculated
- /*
-    for (int i = 0 ; i < N ; i++ ){
-        for (int j = 0 ; j < M ; j++){
-    std::cout << grid[i][j];
+ 
+    for (int i = 0 ; i < height ; i++ ){
+        for (int j = 0 ; j < width ; j++){
+            std::cout << grid[i][j] << std::endl;
 
         }
     }
-    
 */
+
     
     
-// Go through the array again
-
-// First going through the height of the array minus 1
-// on both ends to discard bottom and top line of constant potential
-// for i from one to N - 1
-
-// Then through the whole width of the array
-// for j from 0 to M
-
-// Set conditions if point is on side of grid to only take 3 points
-
-// If it is on the left, j == 0
-// take points i - 1, i + 1, j + 1
-
-// If it is on the right, j == N - 1
-// take points i - 1, i + 1, j - 1
-
-// Otherwise
-// Take the potential of the four surrounding
-// points and average them.
-// point i - 1, i + 1, j - 1, j + 1
-
-// Change value of point to appropriate average
-
-
-
+// Go through the array again to get potential
 //iterate the process an arbitrary number of times
+for ( int c = 0; c < iterations ; c++ ){ // May replace with a while loop later if bool is used to check for convergence
+    
+    // First going through the height of the array minus 1
+    // on both ends to discard bottom and top line of constant potential
+    for ( int i = 1 ; i < height - 1 ; i++ ){
+     
+        // Then through the whole width of the array
+        for ( int j = 0 ; j < width ; j++ ){
+            
+            // Set conditions if point is on side of grid to only take 3 points
+        
+            // If it is on the left, j == 0
+            // take points i - 1, i + 1, j + 1
+            if ( j == 0 ) grid[i][j] = ( grid[i - 1][j] + grid[i + 1][j] + grid[i][j + 1] ) / 3;
 
-    return 0;
+            // If it is on the right, j == N - 1
+            // take points i - 1, i + 1, j - 1
+            if ( j == width - 1 ) grid[i][j] = ( grid[i - 1][j] + grid[i + 1][j] + grid[i][j - 1] ) / 3;
+
+            // Otherwise
+            // Take the potential of the four surrounding
+            // points and average them.
+            // point i - 1, i + 1, j - 1, j + 1
+            else grid[i][j] = ( grid[i - 1][j] + grid[i + 1][j] + grid[i][j - 1] + grid[i][j + 1] ) / 4;
+
+            }// end of vertical scan
+        
+        }//end of horizontal scan
+    
+} //end of itertions
+    
+    
+    
+/*
+    // For testing, print the array calculated
+    
+    for (int i = 0 ; i < height ; i++ ){
+        for (int j = 0 ; j < width ; j++){
+            std::cout << grid[i][j] << std::endl;
+            
+        }
+    }
+    
+
+    
+    
+    return 0; //end main func
 }
+*/
+
+
 
 //comments:
 /*
+ -find a way to output in python retreivable formatan
+ 
  -break tasks down into functions to call from the main file
  
  -start with a finite number of iterations before moving on to
