004b6b1e91de9c152218fe6552ac4379d8cfd7f0
gdideron <guillaumedideron@Gmail.com>
2015-05-11 12:31:14 +0100

    Display comma between array values

--- displayArray.cpp
+++ displayArray.cpp
@@ -1,14 +1,14 @@
 #include <iostream>
 
-void displayArray( double ** array, unsigned heigth, unsigned width ) { // taking array pointing to pointers pointing to double and dimesions
+void displayArray( double ** array, unsigned height, unsigned width ) { // taking array pointing to pointers pointing to double and dimesions
     
-    for( int i = 0 ;  i < heigth ; ++ i ) {
+    for( int i = 0 ;  i < height ; ++ i ) { // sweeping through the whole array vertically
         
         std::cout << std::endl; // Break line for every new row
         
-        for( int j = 0 ; j < width ; ++j ) {
+        for( int j = 0 ; j < width ; ++j ) { // sweeping through the whole array horizontally
             
-            std:: cout << array[i][j] << " " ; // Display the cell values for a row seperated by spaces
+            std:: cout << array[i][j] << ", " ; // Display the cell values for a row seperated by spaces and commas
             
         }
     }
